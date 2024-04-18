a3b944571bdc2043455f707fb09816e5873cf2b8
VanessaTechPro <jackitasoriano@icloud.com>
2024-04-17 19:46:10 -0500

    Final Menu Commit

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 2.please follow the instructions file posted on canvas
 3.Upload your work to canvas and push all your files to your repository.
 4.clone this repository and start working on the Assignmnet-3
+
+I changed the format of menu to be more organized and cleaner. I used sections to seperate the drinks, food, and desserts.
+I added the Javascript for the pop-up order section. I used the order now button and implemented a openOrder function to create the pop effect. 
+I set the order from to only include order aboce 0 and below or equal to ten to avoid quanitity errors and math errors.
+I added alerts to let the consumer know the amounts allowed along with a succesful alert with information such as the item, the quanity, and the total they have ordered. 
--- js/menu.js
+++ js/menu.js
@@ -5,7 +5,7 @@ function openOrder(itemName, price) {
     document.getElementById("itemName").innerHTML = itemName;
     document.getElementById("cartForm").style.display = "block";
     document.getElementById("sumbitButton").onclick = function() {
-        submitOrder(itemNamem, price);
+        submitOrder(itemName, price);
     };
 }
 
@@ -13,16 +13,16 @@ function closeCart() {
     document.getElementById("cartForm").style.display = "none";
 }
 
-function sumbitOrder(itemName, price) {
+document.getElementById("closeCart").addEventListener("click", closeCart);
+
+function submitOrder(itemName, price) {
     var quantity = parseInt(document.getElementById('quantity').value);
 
     if (quantity > 0 && quantity <= 10) {
         var totalPrice = quantity * parseFloat(price);
-        alert('Order submitted successfully!\nItem' + itemName + "\Quantity:" + quantity + "\nTotal Price: $" + totalPrice.toFixed(2));
+        alert('Order submitted successfully!\nItem: ' + itemName + "\nQuantity: " + quantity + "\nTotal Price: $" + totalPrice.toFixed(2));
         closeCart();
     } else {
         alert("Please enter a valid quantity (1-10). Thank you!");
     }
 }
-
-document.getElementById("closeCart").addEventListener("click", closeCart);
